2456ea1b6db61b2e7a6c8087dbad8d621c142e10
Jobcus <jobcusai@gmail.com>
2025-05-27 18:40:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -110,5 +110,6 @@ def get_jobs():
 
     return jsonify({"remotive": remotive_jobs, "adzuna": adzuna_jobs})
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    if __name__ == "__main__":
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host="0.0.0.0", port=port)
