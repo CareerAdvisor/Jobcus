2819ebb60174c8f700cb66e586ed3cdeb06e3efd
Jobcus <jobcusai@gmail.com>
2025-07-06 14:47:38 +0100

    Update chat.js

--- templates/chat.js
+++ templates/chat.js
@@ -182,4 +182,8 @@ function displayJobs(data) {
   }
 
   aiAnswerBlock.appendChild(jobsContainer);
-}
+
+  function toggleMobileMenu() {
+    const menu = document.getElementById("mobileMenu");
+    menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+  }
