936e0b7fbea0a1b5ffc2ce39251619f865585c70
Jobcus <jobcusai@gmail.com>
2025-09-01 10:16:23 +0100

    Delete static directory

--- static/js/resume-common.js
+++
@@ -1,5 +0,0 @@
-// static/js/resume-common.js
-export function initCommon(root) {
-  // Example: smooth scroll for in-resume links, copy-to-clipboard, etc.
-  // Everything should be scoped to `root`.
-}
