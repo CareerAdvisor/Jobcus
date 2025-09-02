28b7108ac0f397b25a7779021eb0e0f309868c82
Jobcus <jobcusai@gmail.com>
2025-09-02 08:27:54 +0100

    Update resume-common.js

--- jobcus/static/js/resume-common.js
+++ jobcus/static/js/resume-common.js
@@ -1,4 +1,3 @@
-// static/js/resume-common.js
 export function initCommon(root) {
   // Example: smooth scroll for in-resume links, copy-to-clipboard, etc.
   // Everything should be scoped to `root`.
