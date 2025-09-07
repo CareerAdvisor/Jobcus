8d068a25bc05999822f2f299a9f4ac831244150d
Jobcus <jobcusai@gmail.com>
2025-09-07 03:34:30 +0100

    Update base.js

--- static/js/base.js
+++ static/js/base.js
@@ -87,7 +87,7 @@ window.autoResize = window.autoResize || function (ta) {
 // CSRF cookie reader (Flask-WTF default cookie names vary; adjust if needed)
 function getCookie(name) {
   const m = document.cookie.match(
-    new RegExp('(?:^|; )' + name.replace(/([.$?*|{}()[\\]\\/+^])/g,'\\$1') + '=([^;]*)')
+    new RegExp('(?:^|; )' + name.replace(/([.$?*|{}()[\]\\/+^])/g, '\\$1') + '=([^;]*)')
   );
   return m ? decodeURIComponent(m[1]) : null;
 }
